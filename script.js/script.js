b9281e6427f49f466925b6a91572320d07387a3f
Optica Aqua <mahoo.stoore@gmail.com>
2026-09-18 11:19:18 -0600

    Actualiza diseño, portada y catalogo

--- script.js/script.js
+++ script.js/script.js
@@ -3,25 +3,25 @@ const lentes = [
         nombre: "Aqua 01",
         tipo: "Armazón oftálmico",
         precio: 899,
-        imagen: "img/lente1.png"
+        imagen: "imagenes/01.png"
     },
     {
         nombre: "Aqua 02",
         tipo: "Armazón oftálmico",
         precio: 799,
-        imagen: "img/lente2.png"
+        imagen: "imagenes/02.png"
     },
     {
         nombre: "Aqua 03",
         tipo: "Armazón de sol",
         precio: 949,
-        imagen: "img/lente3.png"
+        imagen: "imagenes/03.png"
     },
     {
         nombre: "Aqua 04",
         tipo: "Armazón oftálmico",
         precio: 850,
-        imagen: "img/lente4.png"
+        imagen: "imagenes/04.png"
     }
 ];
 
